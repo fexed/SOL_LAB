d7a75874e9f0b03537ead60b9975f619baaae378
Federico Matteoni <federicomatteoni@live.it>
2019-02-26 09:52:17 +0100

    commenting

--- tokenizer/tokenizer.c
+++ tokenizer/tokenizer.c
@@ -8,16 +8,16 @@ int main(int argc, char *argv[]) {
     }
 
     char* saveptr = malloc(sizeof(char*));
-    char* token1 = strtok_r(argv[1], " ", saveptr);
+    char* token1 = strtok_r(argv[1], " ", saveptr); //saveptr per poter tokenizzare mentre tokenizzo altra stringa
     char* secondastringa = malloc(sizeof(argv[2]));
-    secondastringa = strcpy(secondastringa, argv[2]);
+    secondastringa = strcpy(secondastringa, argv[2]); //per poter tokenizzare più volte la stessa stringa
     while (token1) {
 	printf("%s\n", token1);
 	char* saveptr2 = malloc(sizeof(char*));
-	char* token2 = strtok_r(secondastringa, " ", saveptr2);
+	char* token2 = strtok(secondastringa, " ");
 	while(token2) {
 	    printf("%s\n", token2);
-	    token2 = strtok_r(NULL, " ", saveptr2);
+	    token2 = strtok(NULL, " ");
 	}
 	secondastringa = strcpy(secondastringa, argv[2]);
 	token1 = strtok_r(NULL, " ", saveptr);
