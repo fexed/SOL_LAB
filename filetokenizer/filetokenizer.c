d5d2f615271e96d60f8b73fab48e49b2eb0702bf
Federico Matteoni <federicomatteoni@live.it>
2019-02-26 13:12:49 +0100

    Usage

--- filetokenizer/filetokenizer.c
+++ filetokenizer/filetokenizer.c
@@ -1,3 +1,8 @@
-void main(int argc, char *argv[]) {
-	
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	if (argc < 3) {
+		printf("usage: %s filein fileout [append]", argv[0]);
+		return 0;
+	}
 }
