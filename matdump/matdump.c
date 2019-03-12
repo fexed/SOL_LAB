196b7bdaf75e5b170d7f7f1ad5b3d310bbe3f87f
Federico Matteoni <federicomatteoni@live.it>
2019-03-12 11:37:58 +0100

    New exercise

---
+++ matdump/matdump.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define CHECK_PTR(M, string) \
+	if (M == NULL) { \
+		perror(string); \
+		return -1; \
+	}
+
+int main (int argc, char* argv[]) {
+	int N, i, j;
+	float* M;
+	FILE* outfile_txt;
+	FILE* outfile_bin;
+
+	if (argc < 2) {
+		printf("usage: %s N\n", argv[0]);
+		return -1;
+	}
+	N = strtol(argv[1], NULL, 10);
+	M = malloc(N*N*sizeof(float));
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < N; j++) {
+			M[(i*N)+j] = (i+j)/2.0;
+			printf("%.2f\t", M[(i*N)+j]);
+		}
+		printf("\n");
+	}
+
+	outfile_txt = fopen("matdump.txt", "w");
+	CHECK_PTR(outfile_txt, "outfile_txt");
+	outfile_bin = fopen("matdump.dat", "w");
+	CHECK_PTR(outfile_bin, "outfile_bin");
+
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < N; j++) {
+			fprintf(outfile_txt, "%.2f\t", M[(i*N)+j]);
+		}
+		fprintf(outfile_txt, "\n");
+	}
+	fclose(outfile_txt);
+
+	fwrite(M, sizeof(float), N*N, outfile_bin);
+	fclose(outfile_bin);
+
+	free(M);
+	return 0;
+}
