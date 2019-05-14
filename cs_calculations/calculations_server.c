deb3cc5b35c186a3e9892cfdfdd23acc4b396ecb
Federico Matteoni <federicomatteoni@live.it>
2019-05-14 13:28:56 +0200

    Working versions, need to valgrind

--- cs_calculations/calculations_server.c
+++ cs_calculations/calculations_server.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <pthread.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -9,10 +10,27 @@
 #define SOCKETNAME "./calzino"
 #define BUFFSIZE 100
 
+#define CHECK_VALUE(value, name) \
+	if (value != 0) { \
+		printf("Errore creazione thread %s\n", name); \
+	}
+
+static void* calcolatore(void* arg) {
+	char* program = calloc(BUFFSIZE, sizeof(char));
+	program = strcpy(program, "echo \"");
+	program = strcat(program, (char *) arg);
+	program = strcat(program, "\" | bc > output");
+	//strcat(strcat("echo \"", (char*) arg), "\" | bc > output"); 
+	system(program);
+	pthread_exit(NULL);
+}
+
 int main(int argc, char *argv[]) {
-	int skt, skt_accepted;
+	int skt, skt_accepted, value;
 	char buff[BUFFSIZE];
 	struct sockaddr_un skta;
+	pthread_t calcthreadid;
+	FILE* outputfile;
 
 	strncpy(skta.sun_path, SOCKETNAME, UNIX_PATH_MAX);
 	skta.sun_family = AF_UNIX;
@@ -24,8 +42,25 @@ int main(int argc, char *argv[]) {
 		skt_accepted = accept(skt, NULL, 0);
 		do {
 			read(skt_accepted, buff, BUFFSIZE);
-			printf("Server riceve: %s\n", buff);
-			write(skt_accepted, "Sayoonara!", 11);
+			if (strcmp(buff, "exit") != 0) {
+				printf("Server riceve: %s\n", buff);
+				value = pthread_create(&calcthreadid, NULL, *calcolatore, buff);
+				if (value != 0) {
+					printf("Errore creazione thread calcolatore\n");
+					write(skt_accepted, "Errore creazione thread calcolatore", 36);
+				} else {
+					pthread_join(calcthreadid, NULL);
+					outputfile = fopen("output", "r");
+					if (outputfile == NULL) {
+						printf("Errore apertura file output");
+						write(skt_accepted, "Errore apertura file output", 28);
+					} else {
+						fread(buff, BUFFSIZE, sizeof(char), outputfile);
+						fclose(outputfile);
+						write(skt_accepted, buff, BUFFSIZE);
+					}
+				}
+			}
 		} while (strcmp(buff, "exit") != 0);
 		close(skt_accepted);
 	} while (1);	
