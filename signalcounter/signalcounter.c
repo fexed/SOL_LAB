42cd9a8fc974cc4b3d11b068f1181094e79bdc85
Federico Matteoni <federicomatteoni@live.it>
2019-05-25 14:08:12 +0200

    Signal handling working correctly

--- signalcounter/signalcounter.c
+++ signalcounter/signalcounter.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+
+int sigintcounter = 0;
+int sigstpcounter = 0;
+
+static void sigintgestore(int signum) {
+	sigintcounter += 1;
+}
+
+static void sigstpgestore(int signum) {
+	sigstpcounter += 1;
+}
 
 int main() {
+	struct sigaction sig_stop;
+	struct sigaction sig_int;
 	
+	memset(&sig_stop, 0, sizeof(sig_stop));
+	memset(&sig_int, 0, sizeof(sig_int));
+	sig_stop.sa_handler = sigstpgestore;
+	sig_int.sa_handler = sigintgestore;
+	sigaction(SIGINT, &sig_int, NULL);
+	sigaction(SIGTSTP, &sig_stop, NULL);
+
+	printf("SIGINT\tSIGTSTP\n\n");
+	do {
+		sleep(1);
+		printf("%d\t%d\n", sigintcounter, sigstpcounter);
+	} while(sigintcounter < 3);
+
 	return 0;
 }
