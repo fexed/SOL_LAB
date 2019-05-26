0ff341fefb33fde88b8818e44c98bfac0fb71407
Federico Matteoni <federicomatteoni@live.it>
2019-05-26 20:34:56 +0200

    Correctly working A bit of a hack perhaps, but looks good

--- signalcounter/signalcounter.c
+++ signalcounter/signalcounter.c
@@ -2,14 +2,23 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <string.h>
+#include <unistd.h>
 #include <signal.h>
 
 int sigintcounter = 0;
 int sigstpcounter = 0;
 
 static void* waiter(void* arg) {
-	sleep(10);
+	alarm(10);
 	pthread_exit(NULL);
+}
+
+static void sigalrmgestore(int signum) {
+	write(1, "Terminazione", 13);
+	exit(0);
+}
+
+static void sigalrmgestoreignora(int signum) {
 }
 
 static void sigintgestore(int signum) {
@@ -34,16 +43,20 @@ int main() {
 	char* input;
 	struct sigaction sig_stop;
 	struct sigaction sig_int;
+	struct sigaction sig_alrm;
 	pthread_t waiterpid;
 	
 	memset(&sig_stop, 0, sizeof(sig_stop));
 	memset(&sig_int, 0, sizeof(sig_int));
+	memset(&sig_alrm, 0, sizeof(sig_alrm));
 	sig_stop.sa_handler = sigstpgestore;
 	sig_int.sa_handler = sigintgestore;
 	sigaction(SIGINT, &sig_int, NULL);
 	sigaction(SIGTSTP, &sig_stop, NULL);
 
 	do {
+		sig_alrm.sa_handler = sigalrmgestoreignora;
+		sigaction(SIGALRM, &sig_alrm, NULL);
 		printf("\n\t!!!\n");
 		sigstpcounter = 0;
 		do {
@@ -52,9 +65,10 @@ int main() {
 
 		printf("Scrivi \"yes\" per far continuare il programma\n");
 		value = pthread_create(&waiterpid, NULL, *waiter, NULL);
-		input = "";
-		scanf("%s", &input);
-		pthread_join(waiterpid, NULL);
-	} while (strcmp("yes", input) == 0);
+		sig_alrm.sa_handler = sigalrmgestore;
+		sigaction(SIGALRM, &sig_alrm, NULL);
+		input = calloc(10, sizeof(char));
+		scanf("%s", input);
+	} while (strcmp(input, "yes") == 0);
 	return 0;
 }
