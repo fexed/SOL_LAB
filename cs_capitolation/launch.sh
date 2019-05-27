8b8350411dd8933b73de5d13c31060e4f8824381
Federico Matteoni <federicomatteoni@live.it>
2019-05-27 13:17:39 +0200

    Edited script

--- cs_capitolation/launch.sh
+++ cs_capitolation/launch.sh
@@ -1,4 +1,5 @@
-gnome-terminal "./capitolations_server" &
+./capitolations_server &
 ./capitolations_client &
 ./capitolations_client asd &
-./capitolations_client prova "stringa lunga"
+./capitolations_client prova "stringa lunga" &
+./capitolations_client tante parole da capitolare uppercase lowercase more like taunt case
